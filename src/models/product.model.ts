2d9a058d843c7b5bc3824f5cd91dc6cb0f1ea79e
Eduardo Aihara <eaihara90@gmail.com>
2023-11-27 11:22:16 -0300

    feat: add product model

---
+++ src/models/product.model.ts
@@ -0,0 +1,30 @@
+export class ProductModel {
+  private id?: string;
+  private name!: string;
+  private price!: number;
+  private quantity!: number;
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public getPrice(): number {
+    return this.price;
+  }
+
+  public setPrice(price: number): void {
+    this.price = price;
+  }
+
+  public getQuantity(): number {
+    return this.quantity;
+  }
+
+  public setQuantity(quantity: number): void {
+    this.quantity = quantity;
+  }
+}
