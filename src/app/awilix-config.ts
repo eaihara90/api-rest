a5cff8a2ff6e94c8a6bed298da818ade70884d61
Eduardo Aihara <eaihara90@gmail.com>
2023-11-27 15:05:27 -0300

    feat: awilix configuration

---
+++ src/app/awilix-config.ts
@@ -0,0 +1,12 @@
+import { createContainer, InjectionMode, asClass } from 'awilix';
+
+import ProductsController from '@/controllers/products.controller';
+import { ProductsRepository } from '@/repositories/products.repository';
+
+export const container = createContainer({
+  injectionMode: InjectionMode.CLASSIC
+})
+.register({
+  productsController: asClass(ProductsController),
+  productsRepository: asClass(ProductsRepository).scoped()
+});
