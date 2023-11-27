a3ee4d6d2acea1cd26e5082dd3fb17a98a77032f
Eduardo Aihara <eaihara90@gmail.com>
2023-11-27 19:51:58 -0300

    refactor: config constants

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,13 +2,14 @@ import 'dotenv/config';
 
 export default {
   server: {
-    name: 'LOCALHOST',
+    NAME: 'LOCALHOST',
     PORT: process.env.PORT || 3004
   },
   db: {
-    host: process.env.DB_HOST,
-    port: process.env.PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    HOST: process.env.DB_HOST,
+    PORT: process.env.DB_PORT,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    NAME: process.env.DB_NAME
   }
 }
