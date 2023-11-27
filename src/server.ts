7a96420d067d34d0bdb7b95b11e06c5408886348
Eduardo Aihara <eaihara90@gmail.com>
2023-11-27 11:32:00 -0300

    fix: start log

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import config from '@/app/config';
 const server = http.createServer(application.getInstance());
 
 server.listen(config.server.PORT, () => {
-  console.log(`Server running on config.server.PORT ${config.server.PORT}`)
+  console.log(`Server running on port ${config.server.PORT}`)
 });
 
 // application.run(config.server.PORT);
