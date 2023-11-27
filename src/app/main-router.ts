f2a5ab64259ebc06f99475bd9f787c46aa9b2cbb
Eduardo Aihara <eaihara90@gmail.com>
2023-11-27 09:57:40 -0300

    feat: main router

---
+++ src/app/main-router.ts
@@ -0,0 +1,16 @@
+import express, { Router } from "express";
+
+class MainRouter {
+  private readonly instance: Router;
+
+  constructor() {
+    this.instance = express.Router();
+    this.handler.get('', (req, res) => res.send('api works'));
+  }
+
+  public get handler(): Router {
+    return this.instance;
+  }
+}
+
+export default new MainRouter();
