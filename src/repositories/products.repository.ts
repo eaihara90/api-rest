c077241346b484d2cbae379469d45c72cfd6c7f1
Eduardo Aihara <eaihara90@gmail.com>
2023-11-28 18:33:29 -0300

    feat: add find and save methods to repository

--- src/repositories/products.repository.ts
+++ src/repositories/products.repository.ts
@@ -1,13 +1,38 @@
 import { DbConnection } from '@/db/db-connection';
+import { InputProductDTO, OutputProductDTO } from '@/dto/product.dto';
 import { IRepository } from '@/interfaces/IRepository';
 import { ProductModel } from '@/models/product.model';
 
 export default class ProductsRepository implements IRepository<ProductModel> {
   constructor(private readonly db: DbConnection) { }
 
-  public async findAll(): Promise<ProductModel[]> {
+  public async findAll(): Promise<OutputProductDTO[]> {
     try {
-      return this.db.query('SELECT * FROM tb_product;');
+      return this.db.query('SELECT * FROM tb_products;');
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async findById(id: string): Promise<OutputProductDTO> {
+    try {
+      return this.db.query(`SELECT * FROM tb_products WHERE id = '${id}';`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async save(product: InputProductDTO): Promise<OutputProductDTO> {
+    try {
+      await this.db.query(`INSERT INTO tb_products(id, name, price, quantity) VALUES ('${product.getId()}', '${product.getName()}', '${product.getPrice()}', '${product.getQuantity()}');`);
+      
+      const outputProductDTO = new OutputProductDTO();
+      outputProductDTO.setId(product.getId());
+      outputProductDTO.setName(product.getName());
+      outputProductDTO.setPrice(product.getPrice());
+      outputProductDTO.setQuantity(product.getQuantity());
+      
+      return outputProductDTO;
     } catch (error) {
       throw error;
     }
