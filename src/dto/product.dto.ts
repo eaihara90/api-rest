1497bae5acc31a0c001c3befa86889d73dd86567
Eduardo Aihara <eaihara90@gmail.com>
2023-11-28 18:32:11 -0300

    feat: add product I/O DTO

---
+++ src/dto/product.dto.ts
@@ -0,0 +1,13 @@
+import { ProductModel } from '@/models/product.model';
+
+export class InputProductDTO extends ProductModel {
+  constructor() {
+    super();
+  }
+}
+
+export class OutputProductDTO extends ProductModel {
+  constructor() {
+    super();
+  }
+}
