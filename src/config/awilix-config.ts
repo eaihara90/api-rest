0a083982ee128ee02fb473b5017710353c11278f
Eduardo Aihara <eaihara90@gmail.com>
2023-11-30 13:45:33 -0300

    feat: add service class

--- src/config/awilix-config.ts
+++ src/config/awilix-config.ts
@@ -3,12 +3,14 @@ import { createContainer, InjectionMode, asClass, asValue } from 'awilix';
 import ProductsController from '@/controllers/products.controller';
 import ProductsRepository from '@/repositories/products.repository';
 import dbConnection from '@/db/db-connection';
+import ProductsService from '@/services/products.service';
 
 export const container = createContainer({
   injectionMode: InjectionMode.CLASSIC
 })
 .register({
   productsController: asClass(ProductsController),
-  productsRepository: asClass(ProductsRepository).scoped(),
+  productsService: asClass(ProductsService),
+  productsRepository: asClass(ProductsRepository),
   db: asValue(dbConnection)
 });
