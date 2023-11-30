8b1260eb13b7cd1f846e901a4fa44a91f866b1a9
Eduardo Aihara <eaihara90@gmail.com>
2023-11-30 13:45:01 -0300

    refactor: change product model

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,38 +1,8 @@
 export class ProductModel {
-  private id?: string;
-  private name!: string;
-  private price!: number;
-  private quantity!: number;
+  public id!: string;
+  public name!: string;
+  public price!: number;
+  public quantity!: number;
+  constructor() {}
 
-  public getId(): string {
-    return this.id! ;
-  }
-
-  public setId(id: string): void {
-    this.id = id;
-  }
-
-  public getName(): string {
-    return this.name;
-  }
-
-  public setName(name: string): void {
-    this.name = name;
-  }
-
-  public getPrice(): number {
-    return this.price;
-  }
-
-  public setPrice(price: number): void {
-    this.price = price;
-  }
-
-  public getQuantity(): number {
-    return this.quantity;
-  }
-
-  public setQuantity(quantity: number): void {
-    this.quantity = quantity;
-  }
 }
